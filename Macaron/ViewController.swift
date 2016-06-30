6884da4c3b3733c2dba28b0296de6af5181e2e5c
Kohei Tabata <nerd0geek1@gmail.com>
2016-06-30 21:11:02 +0900

    add project

---
+++ Macaron/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Macaron
+//
+//  Created by Kohei Tabata on 6/30/16.
+//  Copyright © 2016 Team Hasuike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
