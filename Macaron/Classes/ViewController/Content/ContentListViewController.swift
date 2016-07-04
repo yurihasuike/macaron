02b5de05661dcbd50cd62157b625d791c117022d
Kohei Tabata <nerd0geek1@gmail.com>
2016-07-04 21:05:29 +0900

    add ContentListViewController

---
+++ Macaron/Classes/ViewController/Content/ContentListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContentListViewController.swift
+//  Macaron
+//
+//  Created by Kohei Tabata on 7/4/16.
+//  Copyright © 2016 Team Hasuike. All rights reserved.
+//
+
+import UIKit
+
+class ContentListViewController: UIViewController {
+
+}
